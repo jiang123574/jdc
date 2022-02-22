6fb7c74afa8b3663070b049fad8f463363ff219b
jiangjiangtao <jiang123574@163.com>
2022-02-22 17:10:13 +0800

    修复bug

--- re_id.py
+++ re_id.py
@@ -1,6 +1,17 @@
 import sqlapi
 import GlobalVariable
 import jdcapi
-GlobalVariable.headers["wskey"] = GlobalVariable.WSKEY
-GlobalVariable.service_headers["tgt"] = GlobalVariable.WSKEY
+import configparser
+
+
+def config():
+    cp = configparser.RawConfigParser()
+    cp.read('config.ini')
+    sqlservice = cp.get('sql', 'sql')
+    tgt = cp.get('jdc', 'WSKEY')
+    return sqlservice, tgt
+
+
+GlobalVariable.headers["wskey"] = config()[1]
+GlobalVariable.service_headers["tgt"] = config()[1]
 print(sqlapi.read_sql("name", "in", 'replace', jdcapi.getListAllUserDevices()))
